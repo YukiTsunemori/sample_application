e49f1f69967617fe424e7c0100816bce115e4426
YukiTsunemori <yuki.tsunemori1229@gmail.com>
2025-01-25 10:42:19 +0000

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
+    # render ...
+    # アクション直下にレンダリングさせるファイルを指定できる
+    # renderなどの指示がない場合、app/views/static_pages/home.html.erbが返される=>デフォルト
+    @time = Time.current.in_time_zone('Asia/Tokyo')
   end
 
   def help
   end
+
+  def about
+    @hello = "This is about page mother fucker"
+  end
+
+
 end
+
+
+
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,30 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  # test "should get root" do
+  #   get （コードを書き込む）
+  #   assert_response （コードを書き込む）
+  # end
+  
   test "should get home" do
-    get static_pages_home_url
+    # GET /static_pages/home
+    get home_url
     assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    # assertは何かが返ってくるべき=>successとなる。という定義文
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_url
     assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
+
+  test "should get about" do
+    get about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+  
 end
