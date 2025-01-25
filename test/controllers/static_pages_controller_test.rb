2d99264e18b4cf1f24942aca90cd0b2a08e74bf8
YukiTsunemori <yuki.tsunemori1229@gmail.com>
2025-01-25 14:39:06 +0000

    commit!

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,20 +11,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     # GET /static_pages/home
     get home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
     # assertは何かが返ってくるべき=>successとなる。という定義文
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
   
 end
