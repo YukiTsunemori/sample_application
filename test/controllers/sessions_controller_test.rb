ac945d39eb80332ea5fc4de10df40289801ca558
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-03-03 02:04:04 +0900

    Modify test method

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,23 @@
 require "test_helper"
 
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+  test "login with invalid information" do
+    # ログインURLへ行く
     get login_path
-    assert_response :success
+    # sessionsコントローラのnewテンプレートが描画される
+    assert_template 'sessions/new'
+    # からのアドレス・パスワードが入る
+    post login_path, params: { session: { email: "", password: "" } }
+    # 失敗する
+    assert_response :unprocessable_entity
+    # 再度ログイン画面が描画される
+    assert_template 'sessions/new'
+    # フラッシュが空ではないですよね？
+    assert_not flash.empty?
+    # 一旦ホームに戻る => メッセージがエラーでまだ表示されることを確認する
+    get root_path
+    # フラッシュがまだ残っていますよね => エラーで残っていることを確認する
+    assert flash.empty?
   end
 end
