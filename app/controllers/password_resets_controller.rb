74205f7b12ab2d378baa50f81c20a8290145c7e9
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-03-15 11:27:49 +0900

    Implement password-reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,7 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user,   only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
   def new
   end
@@ -19,10 +20,28 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  end
 
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can`t be empty")
+      render "edit", status: :unprocessable_entity
+    elsif @user.update(user_params)
+      reset_session
+      log_in @user
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render "edit", status: :unprocessable_entity
+    end
   end
 
   private
+    def user_params
+      # パスワードとパスワード再確認のハッシュのみ、入ることを許可する
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+
     def get_user
       @user = User.find_by(email: params[:email])
     end
@@ -31,6 +50,14 @@ class PasswordResetsController < ApplicationController
     def valid_user
       unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
         redirect_to root_url
+      end
+    end
+
+    # トークンが有効期限切れかどうか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
       end
     end
 end
