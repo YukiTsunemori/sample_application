c82a12b970eb206847aa78c189ad57ab8ea28472
Tsunemori <yuki.tsunemori1229@gmail.com>
2025-03-15 10:54:54 +0900

    12.3.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord # rubocop:disable Style/Documentation
+class User < ApplicationRecord
   # getterとsetterメソッドを使えるようにする
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -76,6 +76,20 @@ class User < ApplicationRecord # rubocop:disable Style/Documentation
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
+
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+
   private
     # メールアドレスを全て小文字にする
     # あとでコールバック関数として呼び出される
